9debdcf3c9c595d9903de48afa31b2b70102b41f
Mehrzad Naghiei <mehrzad.mahbod@gmail.com>
2024-08-28 19:50:02 +0330

    biography added

--- src/app/+pages/biography/biography.component.ts
+++ src/app/+pages/biography/biography.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-biography',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule,MatIconModule],
   templateUrl: './biography.component.html',
   styleUrl: './biography.component.scss'
 })
