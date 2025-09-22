6829710bde81a282094c6dd417550f501a208ca8
Mehrzad Naghiei <mehrzad.mahbod@gmail.com>
2025-09-22 22:46:38 +0330

    Tables updated

--- src/app/+pages/awrads/awrads.component.ts
+++ src/app/+pages/awrads/awrads.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
+import { MatCard, MatCardContent } from "@angular/material/card";
 
 @Component({
   selector: 'app-awrads',
   standalone: true,
-  imports: [MatIconModule],
+  imports: [MatIconModule, MatCard, MatCardContent],
   templateUrl: './awrads.component.html',
   styleUrl: './awrads.component.scss',
 })
--- src/app/+shares/music-player/music-player.component.ts
+++ src/app/+shares/music-player/music-player.component.ts
@@ -10,23 +10,26 @@ import { map, Observable, shareReplay } from 'rxjs';
 
 export interface musicList {
   name: string;
+  role: string;
   position: number;
   duration: string;
   src: string;
   cover: string;
 }
 
-const performances: musicList[] = [
+const musics: musicList[] = [
   {
     position: 1,
     name: 'Azizam',
+    role: 'vocal',
     duration: '5:29',
-    src: './../../../assets/songs/VID_20240323_121714_275.mp3',
+    src: './../../../assets/songs/Azizam.mp3',
     cover: './../../../assets/images/disk.jpg',
   },
   {
     position: 2,
     name: 'Leili Savanam',
+    role: 'vocal',
     duration: '5:01',
     src: './../../../assets/songs/leili bavanam 2-20-24.mp3',
     cover: '',
@@ -34,6 +37,7 @@ const performances: musicList[] = [
   {
     position: 3,
     name: 'Gash Sabakh',
+    role: 'vocal',
     duration: '4:22',
     src: './../../../assets/songs/gash sabakh 2-19-24.mp3',
     cover: '',
@@ -41,6 +45,7 @@ const performances: musicList[] = [
   {
     position: 4,
     name: 'Fasle Gol',
+    role: 'vocal',
     duration: '5:00',
     src: './../../../assets/songs/Fasle Gol 2-17-24.mp3',
     cover: '',
@@ -48,32 +53,105 @@ const performances: musicList[] = [
   {
     position: 5,
     name: 'Hey Binam',
+    role: 'vocal',
     duration: '4:05',
     src: './../../../assets/songs/Hey binam 2-19-24.mp3',
     cover: '',
   },
-];
-
-const vocals: musicList[] = [
+  {
+    position: 6,
+    name: 'Cant help falling in love',
+    role: 'vocal',
+    duration: '2:55',
+    src: './../../../assets/songs/Cant help falling in love.m4a',
+    cover: '',
+  },
+  {
+    position: 7,
+    name: 'Indigo night',
+    role: 'vocal',
+    duration: '4:16',
+    src: './../../../assets/songs/Indigo night .m4a',
+    cover: '',
+  },
+  {
+    position: 8,
+    name: 'Habibi ',
+    role: 'vocal',
+    duration: '5:12',
+    src: './../../../assets/songs/Habibi .m4a',
+    cover: '',
+  },
   {
     position: 1,
-    name: 'Azizam',
-    duration: '5:29',
-    src: './../../../assets/songs/VID_20240323_121714_275.mp3',
-    cover: './../../../assets/images/disk.jpg',
+    name: 'Restless of love',
+    role: 'performance',
+    duration: '7:19',
+    src: './../../../assets/songs/Restless of love.mp3',
+    cover: '',
   },
   {
     position: 2,
-    name: 'Leili Savanam',
-    duration: '5:01',
-    src: './../../../assets/songs/leili bavanam 2-20-24.mp3',
+    name: 'Dashti ( Do zarbi )',
+    role: 'performance',
+    duration: '3:40',
+    src: './../../../assets/songs/Dashti ( Do zarbi ).m4a',
     cover: '',
   },
   {
     position: 3,
-    name: 'Gash Sabakh',
-    duration: '4:22',
-    src: './../../../assets/songs/gash sabakh 2-19-24.mp3',
+    name: 'cheshmeye noosh',
+    role: 'performance',
+    duration: '7:53',
+    src: './../../../assets/songs/cheshmeye noosh.mp3',
+    cover: '',
+  },
+  {
+    position: 4,
+    name: 'shooshtari atr afshan',
+    role: 'performance',
+    duration: '7:04',
+    src: './../../../assets/songs/shooshtari atr afshan.mp3',
+    cover: '',
+  },
+  {
+    position: 5,
+    name: 'Abuata( Ostad Jalil Shahnaz )',
+    role: 'performance',
+    duration: '5:03',
+    src: './../../../assets/songs/Abuata( Ostad Jalil Shahnaz ).m4a',
+    cover: '',
+  },
+  {
+    position: 1,
+    name: 'Eclipse',
+    role: 'electric',
+    duration: '5:03',
+    src: './../../../assets/songs/Eclipse.mp3',
+    cover: '',
+  },
+  {
+    position: 2,
+    name: 'Flow',
+    role: 'electric',
+    duration: '5:03',
+    src: './../../../assets/songs/Flow.mp3',
+    cover: '',
+  },
+  {
+    position: 3,
+    name: 'Spark',
+    role: 'electric',
+    duration: '5:03',
+    src: './../../../assets/songs/Spark.mp3',
+    cover: '',
+  },
+  {
+    position: 4,
+    name: 'Vision',
+    role: 'electric',
+    duration: '5:03',
+    src: './../../../assets/songs/Vision.mp3',
     cover: '',
   },
 ];
@@ -88,25 +166,30 @@ const vocals: musicList[] = [
     FormsModule,
     MatTableModule,
     AsyncPipe,
-  ],
+],
   templateUrl: './music-player.component.html',
   styleUrl: './music-player.component.scss',
 })
 export class MusicPlayerComponent {
   displayedColumns: string[] = ['position', 'name', 'duration'];
-  dataSource = performances;
-  datasource2 = vocals;
+
+  performances = musics.filter(m=> m.role == 'performance');
+  vocals = musics.filter(m=> m.role == 'vocal')
+  electrics = musics.filter(m=> m.role == 'electric')
+
   selectedItem: musicList = {
     name: 'Azizam',
+    role: 'performance',
     position: 1,
     duration: '5:29',
-    src: './../../../assets/songs/VID_20240323_121714_275.mp3',
+    src: './../../../assets/songs/Azizam.mp3',
     cover: './../../../assets/images/disk.jpg',
   };
 
   select(selected: musicList) {
     this.selectedItem = {
       name: selected.name,
+      role: selected.role,
       position: selected.position,
       duration: selected.duration,
       src: selected.src,
